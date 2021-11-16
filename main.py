f66e8b391e6d9e992458ba710fed6c8a5d4d2e01
Andrew Khan <andre-khan@yandex.ru>
2021-11-16 20:34:31 +0300

    изучение условных операторов, тернарного оператора

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # продолжение обучения в Pycharm
+
+name = input("Введите имя ")
+surname = input("Введите фамилию ")
+age = int(input("Введите возраст "))
+
+#Тернарный оператор
+print ("Привет ", name, surname) if age >= 9 else print ("Привет ", name, surname, " тебе еще мало лет")
+
+
+#Условные операторы
+if age >= 9:
+    print ("Привет ", name, surname)
+else:
+    print ("Привет ", name, surname, " тебе еще мало лет")
+print("Привет ", name, surname, ",Ваш возраст", age, "лет")
