0c0b2a3d852c3fbdd7605e1262c6d30924d8252c
Andrew Khan <andre-khan@yandex.ru>
2021-12-05 13:20:08 +0300

    изучение  основы ООП (создание класса и объекта) доп

--- oop.py
+++ oop.py
@@ -14,15 +14,15 @@ class Cat:
 
 cat1 = Cat()
 cat1.set_data("Gad", 4, True)
-# cat1.name = "Gad"
-# cat1.age = 4
-# cat1.isHappy = True
+# cat1.name = "Gad" # вместо этой строки используется set_data
+# cat1.age = 4 # вместо этой строки используется set_data
+# cat1.isHappy = True # вместо этой строки используется set_data
 
 cat2 = Cat()
 cat2.set_data("Murzik", 33, False)
-# cat2.name = "Murzik"
-# cat2.age = 33
-# cat2.isHappy = False
+# cat2.name = "Murzik" # вместо этой строки используется set_data
+# cat2.age = 33 # вместо этой строки используется set_data
+# cat2.isHappy = False # вместо этой строки используется set_data
 
 cat1.get_data()
 cat2.get_data()
